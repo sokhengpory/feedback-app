8a5d4b643b8dec4b99bed187fb7c0c085b5b58fd
Sokheng <porysokheng@gmail.com>
2022-04-18 16:21:03 +0700

    initializing react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// import css to react
+import './index.css';
+
+// import "App" component from "App.js"
+import App from './App';
+
+// redner "App" component as JSX (<App />) to the DOM
+// we can also use react strict mode by wrapping the "<App />" component with "<React.StrictMode>"
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
