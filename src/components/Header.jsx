8eed56ef56e3be3642b88b301fb8e12b8fd212a1
Sokheng <porysokheng@gmail.com>
2022-04-19 16:38:55 +0700

    adding style to the component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,19 @@
 import PropTypes from 'prop-types';
 
 // destruct props property (text) from "props" object
-function Header({ text }) {
+function Header({ text, bgColor, textColor }) {
+  // define style and store it in a variable
+  const headerStyle = {
+    backgroundColor: bgColor,
+    color: textColor,
+  };
+
   return (
-    <header className="continer">
-      <h2>{text}</h2>
+    // use "style" to apply the css to the component
+    <header style={headerStyle}>
+      <div className="container">
+        <h2>{text}</h2>
+      </div>
     </header>
   );
 }
@@ -12,11 +21,15 @@ function Header({ text }) {
 // set defualt props for this component (if no props passed then use the default one)
 Header.defaultProps = {
   text: 'Feedback UI',
+  bgColor: 'rgba(0,0,0,0.4)',
+  textColor: '#ff6a95',
 };
 
 // define types for the props
 Header.propTypes = {
   text: PropTypes.string,
+  bgColor: PropTypes.string,
+  textColor: PropTypes.string,
 };
 
 export default Header;
