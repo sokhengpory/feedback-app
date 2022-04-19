c123c1195a7c680ab65ad8f29bf0669ef219f415
Sokheng <porysokheng@gmail.com>
2022-04-19 15:25:45 +0700

    dynamic values & lists

--- src/App.js
+++ src/App.js
@@ -1,5 +1,41 @@
+// when we return JSX, it must be only one parent element
+// "<></>"" react fragment use when we don't want any div to wrap the child element
+// class => className, for => htmlFor
 function App() {
-  return <h1>Hello from the app component!</h1>;
+  const title = 'Blog Post';
+  const body = 'This is my post';
+  const comments = [
+    {
+      id: 1,
+      text: 'Comment one',
+    },
+    {
+      id: 2,
+      text: 'Comment two',
+    },
+    {
+      id: 3,
+      text: 'Comment three',
+    },
+  ];
+
+  return (
+    <>
+      <div className="container">
+        <h1>{title}</h1>
+        <p>{body}</p>
+
+        <div className="comments">
+          <h3>Comments ({comments.length})</h3>
+          <ul>
+            {comments.map((comment, index) => (
+              <li key={comment.id}>{comment.text}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
 // import css to react
 import './index.css';
@@ -7,11 +7,28 @@ import './index.css';
 // import "App" component from "App.js"
 import App from './App';
 
-// redner "App" component as JSX (<App />) to the DOM
+// OLD REACT USE THIS STYLE TO RENDER MAIN COMPONENT
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// render "App" component as JSX (<App />) to the DOM
 // we can also use react strict mode by wrapping the "<App />" component with "<React.StrictMode>"
-ReactDOM.render(
+// NEW REACT VERSION (18) USE THIS STYLE TO RENDER MAIN COMPONENT
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
+
+// ALTERNATIVE STYLE TO RENDER MAIN COMPONENT
+// const container = document.getElementById('root');
+// const root = ReactDOM.createRoot(container);
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
